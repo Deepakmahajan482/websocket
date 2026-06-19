60626f5fdade90634317a8067d081516bc9366b8
Deepakmahajan482 <Deepakmahajan3316@gmail.com>
2026-06-19 22:47:50 +0530

    final version

--- frontend/src/Message.tsx
+++ frontend/src/Message.tsx
@@ -54,26 +54,30 @@ const Message = () => {
     setText("");
   }
   return (
-   <div className='text-white h-screen bg-black flex flex-col'>
+   <div className='text-white h-screen bg-black flex flex-col p-10'>
     <div className="flex justify-between p-6">
     <div className="p-2 bg-gray-400 rounded-full"> User: {name}</div> <div className="p-2 bg-gray-400 rounded-full">  Room: {room}</div>
     </div>
     <div className='flex-1 overflow-y-auto px-4 py-2'>
       {Message.map(message=><div className="m-8"><span className="bg-white  text-black p-4 rounded">{message}</span></div>)}
     </div>
-    <div className='font-white w-full flex bg-white'>
-      <input value={text} onChange={(e) => setText(e.target.value)} type="text" placeholder='type a message' className='text-black border-none hover:border-none flex-1 p-4'
+    <div className='font-white w-full flex bg-white rounded-full' >
+      <input value={text} onChange={(e) => setText(e.target.value)} type="text" placeholder='type a message' className='rounded-full text-black border-none hover:border-none flex-1 p-4'
        onKeyDown={(e) => {
     if (e.key === "Enter") {
       sendMessage();
     }
   }}>
 
+
+
+
+
   </input>
       <button onClick={()=>{
-   sendMessage;
+   sendMessage();
       }
-    }className=' bg-green-600 p-4 cursor-pointer'><IoMdSend size={32}/></button>
+    }className=' bg-green-600 p-4 cursor-pointer rounded-full'><IoMdSend size={32}/></button>
       </div>
    </div>
   )
